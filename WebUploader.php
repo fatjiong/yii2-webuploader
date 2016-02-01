31cb5da1e25a22d3931117a199254191ac3f40ff
fatjiong <757970599@qq.com>
2016-02-01 16:10:25 +0800

    [*] 更新插件

--- WebUploader.php
+++ WebUploader.php
@@ -15,7 +15,9 @@ class WebUploader extends \yii\base\Widget
 	public $template =  '';
 	public $options = [];
 	public $jsOptions = [];
-	public $thumb = [];
+	public $thumb = [];// 缩略图
+	public $accept = [];// 允许的文件
+	public $status = [];// 允许状态回调
 
 	public function init()
 	{	
@@ -27,8 +29,15 @@ class WebUploader extends \yii\base\Widget
    		$this->jsOptions['auto'] = 'true';
    		$this->jsOptions['swf'] = "'./asset/js/Uploader.swf'";
    		$this->jsOptions['server'] = "'$this->url'";
-   		$this->jsOptions['pick'] = "'#$this->id'";
-	}
+   		$this->jsOptions['pick'] = "{id:'#$this->id',multiple:false}";
+   		$this->jsOptions['accept'] = "{
+										        title: 'Images',
+										        extensions: 'png,jpg,gif,doc,docx,xls,xlsx,pdf,txt',
+										        mimeTypes: 'application/zip,application/vnd.ms-excel,application/vnd.ms-powerpoint,image/*' +
+										            ',application/vnd.openxmlformats-officedocument.presentationml.presentation' +
+										            ',application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/msword' +
+										            ',application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,text/plain,application/x-rar-compressed'}";
+		}
 
     public function run()
     {
@@ -44,16 +53,31 @@ class WebUploader extends \yii\base\Widget
 		foreach ($this->jsOptions as $key => $value) {
 			$script .= "$key : $value,";
 		}
+		if ($this->accept) {
+			if ($this->accept['office']) {
+				$script .= "accept : {".$this->accept['office']."},";
+			}
+		}
 		$script .= "});";
 		if ($this->thumb) {
-			$script .= "uploader.on( 'fileQueued', function( file ) {
-						uploader.makeThumb( file, function( error, ret ) {
-								endThumb(ret); // 生成缩略图后执行方法
-						},".$this->thumb['width'].",".$this->thumb['height'].");
-				 	});";
+			// 可以单独执行缩略图生成base64的缓存图片
+			// $script .= "uploader.on( 'fileQueued', function( file ) {
+			// 			uploader.makeThumb( file, function( error, ret ) {
+			// 					endThumb(ret); // 生成缩略图后执行方法
+			// 			},".$this->thumb['width'].",".$this->thumb['height'].");
+			// 	 	});";
+			$script .= "// 修改后图片上传前，尝试将图片压缩
+			uploader.option('compress', {
+			    width: ".$this->thumb['width'].",
+			    height: ".$this->thumb['height']."
+			});";
 		}
-		$script .= "uploader.on('uploadSuccess', function (file) {
-						endUploader(file);   // 上传完成后执行方法
+
+		in_array('beforeFileQueued', $this->status) && $script .= "uploader.on('beforeFileQueued', function (file) {
+						beforeFileQueued(file);   // 加入队列前执行方法
+					});";
+		$script .= "uploader.on('uploadSuccess', function (file,response) {
+						endUploader(file,response);   // 上传完成后执行方法
 					});";
 		$this->view->registerJs($script, View::POS_READY);
 	}
