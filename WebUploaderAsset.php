1f74d4af9a0b343a269a594f11d5c3623835e87a
fatjiong <757970599@qq.com>
2018-06-06 16:57:46 +0800

    [+] 修改css

--- WebUploaderAsset.php
+++ WebUploaderAsset.php
@@ -1,19 +1,24 @@
 <?php
 namespace fatjiong\webuploader;
+
 use yii\web\AssetBundle;
-class WebUploaderAsset extends AssetBundle {
-    public $js=[
+
+class WebUploaderAsset extends AssetBundle
+{
+    public $css = [
+        'assets/css/webuploader.css',
+    ];
+
+    public $js = [
         'assets/js/webuploader.min.js',
     ];
-    public $jsOptions=[
-        'charset'=>'utf8',
+    public $jsOptions = [
+        'charset' => 'utf8',
     ];
 
-
-    public function init() {
+    public function init()
+    {
         //资源所在目录
         $this->sourcePath = dirname(__FILE__) . DIRECTORY_SEPARATOR;
     }
 }
-
-?>
